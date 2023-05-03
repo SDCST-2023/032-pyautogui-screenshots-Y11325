2ab059da74e0e6587c1331dd11966c9b34f09f62
Amy <amyy290@deltalearns.ca>
2023-05-03 11:45:34 -0700

    update

--- assignment.py
+++ assignment.py
@@ -3,42 +3,35 @@
 import pyautogui as p
 import time
 
+def click():
+    while True:
+        p.click()
+
 def CandyButton():
-    global candies
-    candies = 0
     location = p.locateCenterOnScreen('assets/CANDY.png', confidence = 0.8)
     print('locationOnScreen: ', location)
     if location != None:
         p.moveTo(location)
-        while True:
-            candies += 1
-            p.click()
-            if candies == 15:
-                break
+        p.click()
     return
 
-def autoclick():
-    location = p.locateCenterOnScreen('assets/autoclick.png', confidence = 0.8)
-    print('locationOnScreen: ', location)
-    if location != None:
-        p.confirm(text='do you want to buy this?', title='autoclick', buttons=['OK','Cancel'])
-        p.moveTo(location)
-        p.click()
-        main()
-
-def okbox():
-    p.locateCenterOnScreen('assets/messagebox.png', confidence = 0.8)
-    location = p.locateCenterOnScreen('assets/OK.png', confidence = 0.8)
-    print('locationOnScreen: ', location)
-    if location != None:
-        p.moveTo(location)
-        p.click()
-        main()
+def store():
+    location = []
+    location1 = location.append(p.locateCenterOnScreen('assets/autoclick.png', confidence = 0.8))
+    location2 = location.append(p.locateCenterOnScreen('assets/cursor.png', confidence = 0.8))
+    
+    for i in location:
+        print(i)
+        print('locationOnScreen: ', location)
+        if i != None:
+            p.moveTo(i)
+            p.click()
+    return 
 
 def main():
     CandyButton()
-    autoclick()
-    okbox()
+    store()
 
 if __name__ == "__main__":
+    p.confirm("Click to start")
     main()
