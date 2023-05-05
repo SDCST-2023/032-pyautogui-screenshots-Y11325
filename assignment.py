ad160e229939ee6fa91101c5f87e89aaaa1f504e
Amy <amyy290@deltalearns.ca>
2023-05-05 11:35:21 -0700

    update

--- assignment.py
+++ assignment.py
@@ -4,11 +4,12 @@ import pyautogui as p
 import time
 
 def click():
-    while True:
-        p.click()
+    for i in range(200):
+        p.click()        
+    return
 
 def CandyButton():
-    time.sleep(0.05)
+    time.sleep(0.025)
     location = p.locateCenterOnScreen('assets/CANDY.png', confidence = 0.8)
     print('CANDY_LOCATION: ', location)
     while True:
@@ -16,54 +17,44 @@ def CandyButton():
             location = p.locateCenterOnScreen('assets/CANDY.png', confidence = 0.75)
         elif location != None:
             p.moveTo(location)
-            for i in range(100):
-                p.click()
+            click()
             break
-    
     return
-def store2():
-    candyrocket = (p.locateCenterOnScreen('assets/candyrocket.png', confidence = 0.9))
 
-    diamondcursor = (p.locateCenterOnScreen('assets/diamondcursor.png', confidence = 0.9))
-
-    candytemple = (p.locateCenterOnScreen('assets/candytemple.png', confidence = 0.9))
-
-    candylab = (p.locateCenterOnScreen('assets/candylab.png', confidence = 0.9))
-
-    goldcursor = (p.locateCenterOnScreen('assets/goldcursor.png', confidence = 0.9))
-
-    candyfactory = (p.locateCenterOnScreen('assets/candyfactory.png', confidence = 0.9))
-
-    candymine = (p.locateCenterOnScreen('assets/candymine.png', confidence = 0.9))
-
-    candyfarm = (p.locateCenterOnScreen('assets/candyfarm.png', confidence = 0.9))
-
-    steelcursor = (p.locateCenterOnScreen('assets/steelcursor.png', confidence = 0.9))
-
-    autocandy = (p.locateCenterOnScreen('assets/autocandy.png', confidence = 0.9))
-
-    cursor = (p.locateCenterOnScreen('assets/cursor.png', confidence = 0.9))
-
-    autoclick = (p.locateCenterOnScreen('assets/autoclick.png', confidence = 0.9))
+def images():
+    candyrocket = (p.locateCenterOnScreen('assets/candyrocket.png', confidence = 0.85, grayscale = True))
+    diamondcursor = (p.locateCenterOnScreen('assets/diamondcursor.png', confidence = 0.85, grayscale = True))
+    candytemple = (p.locateCenterOnScreen('assets/candytemple.png', confidence = 0.85, grayscale = True))
+    candylab = (p.locateCenterOnScreen('assets/candylab.png', confidence = 0.85, grayscale = True))
+    goldcursor = (p.locateCenterOnScreen('assets/goldcursor.png', confidence = 0.85, grayscale = True))
+    candyfactory = (p.locateCenterOnScreen('assets/candyfactory.png', confidence = 0.85, grayscale = True))
+    candymine = (p.locateCenterOnScreen('assets/candymine.png', confidence = 0.85, grayscale = True))
+    candyfarm = (p.locateCenterOnScreen('assets/candyfarm.png', confidence = 0.85, grayscale = True))
+    steelcursor = (p.locateCenterOnScreen('assets/steelcursor.png', confidence = 0.85, grayscale = True))
+    autocandy = (p.locateCenterOnScreen('assets/autocandy.png', confidence = 0.85, grayscale = True))
+    cursor = (p.locateCenterOnScreen('assets/cursor.png', confidence = 0.85, grayscale = True))
+    autoclick = (p.locateCenterOnScreen('assets/autoclick.png', confidence = 0.85, grayscale = True))
 
     return candyrocket, diamondcursor, candytemple, candylab, goldcursor, candyfactory, candymine, candyfarm, steelcursor, autocandy, cursor, autoclick
 
-def store(cr,dc,ct,cl,gc,cfac,cm,cfarm,sc,ac,c,aucl):
-    location = [cr,dc,ct,cl,gc,cfac,cm,cfarm,sc,ac,c,aucl]
+def store(cRocket,dCursor,cTemple,cLab,gCursor,cFactory,cMine,cFarm,sCursor,aCandy,Cursor,aClick):
+    location = [cRocket,dCursor,cTemple,cLab,gCursor,cFactory,cMine,cFarm,sCursor,aCandy,Cursor,aClick]
 
     for i in location:
-        if i != None:
-            p.moveTo(i[0],i[1], 0.5)
+        if i == None:
+            pass
+        elif i != None:
+            p.moveTo(i[0],i[1], 0.025)
             p.click()
-        elif i == None:
-            main()
     main()
     return 
 
 def main():
-    CandyButton()
-    cr,dc,ct,cl,gc,cfac,cm,cfarm,sc,ac,c,aucl = store2()
-    store(cr,dc,ct,cl,gc,cfac,cm,cfarm,sc,ac,c,aucl)
+    while True:
+        CandyButton()
+    cRocket,dCursor,cTemple,cLab,gCursor,cFactory,cMine,cFarm,sCursor,aCandy,Cursor,aClick = images()
+    #store(cRocket,dCursor,cTemple,cLab,gCursor,cFactory,cMine,cFarm,sCursor,aCandy,Cursor,aClick)
+    return
 
 if __name__ == "__main__":
     p.confirm("Click to start")
